f8a81974c47bfe2a29c57788c894df2cb116faa3
Adrian Valbuena <adrianva@cloudstaff.com>
2020-09-21 11:50:28 +0800

    core

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('theme-fuse app is running!');
+    expect(page.getTitleText()).toEqual('theme-flight app is running!');
   });
 
   afterEach(async () => {
