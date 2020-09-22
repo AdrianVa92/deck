51675c3857663f55af7177587859d4731b463850
carloco-cloudstaff <carloco@cloudstaff.com>
2020-09-22 11:53:53 +0800

    FD-14 FD-15 FD-17 - grid and list

---
+++ src/app/application-library/application-library-list/application-library-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-application-library-list',
+  templateUrl: './application-library-list.component.html',
+  styleUrls: ['./application-library-list.component.scss']
+})
+export class ApplicationLibraryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/staff-summary/staff-summary-list/staff-summary-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-summary-list',
+  templateUrl: './staff-summary-list.component.html',
+  styleUrls: ['./staff-summary-list.component.scss']
+})
+export class StaffSummaryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
